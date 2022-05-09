7626568cf72c5be8aecf7a4217d6e771ea558233
p4tac0 <104589010+p4tac0@users.noreply.github.com>
2022-05-10 00:14:31 +0200

    Metro.py 1.1.0 Beta Version

--- metro.py
+++ metro.py
@@ -30,6 +30,12 @@ class Access:
     _location : Point    # Posició de tipus punt del accés en qüestió
     _station_id : str    # Identificador de l'estació associada al accés
 
+@dataclass
+class Edge:
+    _type : str                         # Possibilitats: Street, Acces, Enllaç
+    _distance : float                   # Haversine distancia entre node [0] and node [1]
+    _colour_id : str                    # RGB
+
 Stations = List[Station]
 
 Accesses = List[Access]
@@ -61,7 +67,7 @@ def read_stations() -> Stations:
 
     for row in df.itertuples():
         point : Point = get_coordinates(row.GEOMETRY)
-        Current_Station = Station(row.CODI_ESTACIO, row.NOM_ESTACIO ,row.NOM_ESTACIO+" "+row.NOM_LINIA, row.NOM_LINIA,  #El nom de la estació el deixem com a suma del nom i la linia per evitar cicles d'una estació amb si mateixa.
+        Current_Station = Station(row.CODI_ESTACIO, row.NOM_ESTACIO ,row.NOM_ESTACIO+";"+row.NOM_LINIA, row.NOM_LINIA,  #El nom de la estació el deixem com a suma del nom i la linia per evitar cicles d'una estació amb si mateixa.
         '#'+row.COLOR_LINIA, point)       # Nota: sumem '#' al color de línia per a acabar de passar-ho a format RGB
         Stations.append(Current_Station)
 
@@ -90,7 +96,7 @@ def read_accesses() -> Accesses:
 
     for row in df.itertuples():
         coords: Point = get_coordinates(row.GEOMETRY)
-        Current_Access = Access( row.ID_ACCES, row.NOM_ACCES ,row.NOM_ESTACIO,
+        Current_Access = Access( str(row.ID_ACCES), row.NOM_ACCES ,row.NOM_ESTACIO,
         row.NOM_TIPUS_ACCESSIBILITAT, coords , row.ID_ESTACIO)
         Accesses.append(Current_Access)
 
@@ -109,17 +115,17 @@ def show(Metro_Graph: MetroGraph)-> None:
 
 def plot(g : MetroGraph) -> None:
     m = StaticMap(2500, 3000, 80)
-    m = add_lines(m, g)
     m = add_nodes(m, g)
+    m = add_lines(m, g)
     image = m.render()
-    image.save('metro_bcn.png')
+    image.save('bcn.png')
 
 def add_lines(m : StaticMap, g: MetroGraph) -> StaticMap:
     """ Donat un StaticMap i un graf, afegeix les arestes del graf a l'StaticMap """
-    for edge in g.edges():
+    for edge in g.edges:
         edge_attributes = g.get_edge_data(*edge)
-        if edge_attributes["type"]== "Tram": # Si l'aresta en la que estem és de tipus Tram, cal pintar-la del color de la línia a la que pertany.
-            color = edge_attributes["color"]
+        if edge_attributes["attributes"]._type== "Tram": # Si l'aresta en la que estem és de tipus Tram, cal pintar-la del color de la línia a la que pertany.
+            color = edge_attributes["attributes"]._colour_id
         else:                          # En cas contrari, cal pintar la aresta de color gris, Nota: l'hem pintat de color fuchsia per apreciar millor els camins d'access.
             color = "#FF00FF"          # Gris és : "#BABAB6"
         line = Line(edge_attributes["coordinates"], color, 5)  # (Tupla de punts, color, gruix)
@@ -129,13 +135,11 @@ def add_lines(m : StaticMap, g: MetroGraph) -> StaticMap:
 def add_nodes(m : StaticMap, g : MetroGraph) -> StaticMap:
     """ Donat un StaticMap i un graf, afegeix els nodes del graf a l'StaticMap """
     for node in g.nodes():
-        type_node = nx.get_node_attributes(g,"type") # Retorna un diccionari de la forma (identificador_del_node : type)
-        if type_node[node] == "Acces":
-            coords = nx.get_node_attributes(g, "coordinates") # Retorna un diccionari de la forma (identificador_del_node : coordinates)
+        coords = nx.get_node_attributes(g,"coordinates") # Retorna un diccionari de la forma (identificador_del_node : coordinates)
+        if g.nodes[node]["type"] == "Acces":
             marker = CircleMarker(coords[node], "#FF00FF", 4) # (Centre del node, color, radi)
             m.add_marker(marker)
         else:
-            coords = nx.get_node_attributes(g, "coordinates")
             marker = CircleMarker(coords[node], "#2B2B2B", 10)
             m.add_marker(marker)
     return m
@@ -156,7 +160,7 @@ def get_coordinates(info : str) -> Point:
 
 def get_distance(point1: Point, point2 : Point)-> float:
     """ A partir de les coordenades de dos punts, retorna la distancia entre aquests """
-    return hs.haversine(point1, point2)
+    return hs.haversine(point1, point2)*1000
 
 
 # -------------------------------- #
@@ -174,8 +178,13 @@ def add_nodes_and_edges_stations(station1: Station, station2: Station, metro_gra
 
     #Si els dos nodes que hem afegit son de la mateixa línia, els unim amb una aresta de tipus Tram
     if station1._nameline == station2._nameline:  #Si son de la mateixa línia
-        metro_graph.add_edge(station1._compound_name, station2._compound_name, type = "Tram", nameline = station1._nameline,
-        distance = get_distance(station1._location, station2._location), color = station1._linecolor, coordinates = [station1._location, station2._location]) #Atributs: (tipus d'aresta, linia, distancia, color línia)
+        _type = "Tram"
+        _distance = get_distance(station1._location, station2._location)
+        _colour_id = station1._linecolor
+
+        edge = Edge(_type, _distance, _colour_id)
+
+        metro_graph.add_edge(station1._compound_name, station2._compound_name, attributes = edge, coordinates = [station1._location, station2._location]) #Atributs: (tipus d'aresta, linia, distancia, color línia)
 
 
 
@@ -188,16 +197,20 @@ def add_edges_accesses(qty_stations: int, all_stations : Stations, all_accesses 
             if station._id == access._station_id : # Assegurar-nos que l'acces i la estació comparteixen identificador d'estació.
                 first_matching_access = True
 
+                _type = "Acces"
+                _distance = get_distance(station._location, access._location)
+                _colour_id = "BABAB6"
+                edge = Edge ( _type, _distance, _colour_id)
+
                 # Afegim una Aresta de tipus Acces
-                metro_graph.add_edge(station._compound_name, access._id, type = "Acces",distance = get_distance(station._location, access._location),
-                color = station._linecolor, coordinates = [station._location, access._location]) # Atributs: (tipus d'aresta, distancia, color línia)
+                metro_graph.add_edge(station._compound_name, access._id+"Access", attributes = edge, coordinates = [station._location, access._location]) # Atributs: (tipus d'aresta, distancia, color línia)
             elif first_matching_access: # Si ja hem trobat previament un access per a aquella estacio i l'access d'aquesta iteració ja no ho és, significa que ja no n'hi ha més (doncs estan ordenats)
                 break
 
 def add_nodes_accesses(all_accesses: Accesses, metro_graph : MetroGraph)-> None:
     """ Donats tots els accessos, els afegeix com a nodes al graf """
     for access in all_accesses:
-        metro_graph.add_node(access._id, type = "Acces", accessibility = access._accessible, coordinates = access._location) # Afegim node de tipus Acces.
+        metro_graph.add_node(access._id+"Access", type = "Acces", accessibility = access._accessible, coordinates = access._location) # Afegim node de tipus Acces.
 
 def add_transbording_edges(all_stations: Stations, metro_graph: MetroGraph)-> None:
     """ Realitza un recorregut sobre les estacions per veure quines tenen enllaç entre si """
@@ -206,17 +219,25 @@ def add_transbording_edges(all_stations: Stations, metro_graph: MetroGraph)-> No
             if station1._nameline != station2._nameline and station1._name == station2._name: #Si les estacions no comparteixen linia però comparteixen nom, son arestes d'enllaç
 
                 # Afegim aresta d'enllaç al graf
-                metro_graph.add_edge(station1._compound_name, station2._compound_name, type = "Enllaç", nameline = station1._nameline,
-                distance = get_distance(station1._location, station2._location), color = station1._linecolor, coordinates = [station1._location, station2._location]) #Atributs: (tipus d'aresta, linia, distancia, color línia)
+                _type = "Enllaç"
+                _distance = get_distance(station1._location, station2._location)
+                _colour_id = station1._linecolor
+
+                edge = Edge(_type, _distance, _colour_id)
+                metro_graph.add_edge(station1._compound_name, station2._compound_name, attributes  = edge, coordinates = [station1._location, station2._location]) #Atributs: (tipus d'aresta, linia, distancia, color línia)
 
 
 # ----------------------------------- #
 # MÈTODES DEL MAIN I CREACIÓ DEL GRAF #
 # ----------------------------------- #
 
-def get_metro_graph(all_accesses: Accesses, all_stations : Stations) -> MetroGraph:
+def get_metro_graph() -> MetroGraph:
     """ Retorna un graf del metro de Barcelona amb la informació dels arxius accessos.csv i estacions.csv """
     Metro_Graph = nx.Graph() #Inicialitzem un graf buit que contindra tota la informació de metro de barcelona
+
+    #Obtenim la informacio dels accessos i estacions del metro de Barcelona
+    all_accesses = read_accesses()
+    all_stations = read_stations()
 
     qty_stations = len(all_stations)
     for i in range (1, qty_stations):     #Iterem totes les estacions dos a dos repetint sempre una estació
@@ -228,20 +249,12 @@ def get_metro_graph(all_accesses: Accesses, all_stations : Stations) -> MetroGra
     add_edges_accesses(qty_stations, all_stations, all_accesses, Metro_Graph) # Afegim totes les arestes de tipus access entre Access i estació
     add_transbording_edges(all_stations, Metro_Graph)                         # Afegim totes les Arestes d'enllaç entre estació i estació
 
-    return Metro_Graph
-
-
-def main():
-    #Obtenim la informacio dels accessos i estacions del metro de Barcelona
-    all_accesses = read_accesses()
-    all_stations = read_stations()
-    #Obtenim el Graf del Metro de Barcelona
-    Metro_Graph = get_metro_graph(all_accesses, all_stations)
-
     #Mostrem per pantalla el metro del graf de Barcelona
-    show(Metro_Graph)
+    #show(Metro_Graph)
 
     #Generem un png amb el metro de Barcelona dibuixat per sobre.
-    plot(Metro_Graph)
+    #plot(Metro_Graph)
 
-main()
+    return Metro_Graph
+
+#get_metro_graph()
